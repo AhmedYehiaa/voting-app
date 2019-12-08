42870a2613530cc517fdfde7b215e11b384a5222
ahmedyehiaa <ahmedyehia93@gmail.com>
2019-12-08 04:01:41 +0200

    Add react-router-dom with notFound component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import {
+  Route,
+  Redirect,
+  Switch,
+  BrowserRouter as Router
+} from "react-router-dom";
+
 import Header from "./components/Header";
 import Questions from "./containers/Questions";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Questions />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Questions} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </Router>
     </div>
   );
 }
