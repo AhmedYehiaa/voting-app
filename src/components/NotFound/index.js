7ade8fb5ca1b92f02e250af69a033dfb0307b131
ahmedyehiaa <ahmedyehia93@gmail.com>
2019-12-08 04:11:54 +0200

    Update Notfound component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-  return <div>Not found</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.status}>
+        <h1>404</h1>
+      </div>
+      <div className={styles.errorMessage}>
+        <h2>oops! this page could not be found</h2>
+        <Link className={styles.link} to="/">
+          Return to Home Page
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
